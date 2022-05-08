3388165dc92191e84d6727c8010bc535f7e2b666
Xuanmeng Zhang <xuanmengzhang@XuanmengdeMacBook-Air.local>
2022-05-08 11:48:05 +0800

    add files

--- fid_evaluation.py
+++ fid_evaluation.py
@@ -6,6 +6,7 @@ import argparse
 
 from torchvision.utils import save_image
 from pytorch_fid import fid_score
+from torch_fidelity import calculate_metrics
 from tqdm import tqdm
 
 import datasets
@@ -37,40 +38,6 @@ def setup_evaluation(dataset_name, generated_dir, eval_dir, dataset_path, target
     if generated_dir is not None:
         os.makedirs(generated_dir, exist_ok=True)
     return real_dir
-   
-def output_images(alpha, generator, input_metadata, rank, world_size, output_dir, num_imgs=2048):
-    metadata = copy.deepcopy(input_metadata)
-    # metadata['img_size'] = 128
-    metadata['batch_size'] = 2
-
-    metadata['h_stddev'] = metadata.get('h_stddev_eval', metadata['h_stddev'])
-    metadata['v_stddev'] = metadata.get('v_stddev_eval', metadata['v_stddev'])
-    metadata['sample_dist'] = metadata.get('sample_dist_eval', metadata['sample_dist'])
-    metadata['psi'] = 1
-
-    img_counter = rank
-    generator.eval()
-    img_counter = rank
-
-    if rank == 0: pbar = tqdm("generating images", total = num_imgs)
-    with torch.no_grad():
-        while img_counter < num_imgs:
-            z = torch.randn((metadata['batch_size'], generator.module.z_dim), device=generator.module.device)
-            generated_imgs, _ = generator.module.staged_forward(z, alpha, **metadata)
-
-            for img in generated_imgs:
-                save_image(img, os.path.join(output_dir, f'{img_counter:0>5}.jpg'), normalize=True, range=(-1, 1))
-                img_counter += world_size
-                if rank == 0: pbar.update(world_size)
-    if rank == 0: pbar.close()
-
-def calculate_fid(dataset_name, generated_dir, eval_dir, target_size=256):
-    real_dir = os.path.join(eval_dir, 'EvalImages', dataset_name + '_real_images_' + str(target_size))
-    fid = fid_score.calculate_fid_given_paths([real_dir, generated_dir], 128, 'cuda', 2048)
-    # 128 is the batch_size and 2048 is the feature dims of Inception model
-    torch.cuda.empty_cache()
-
-    return fid
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -81,5 +48,6 @@ if __name__ == '__main__':
     parser.add_argument('--num_imgs', type=int, default=8000)
 
     opt = parser.parse_args()
-
     real_images_dir = setup_evaluation(opt.dataset, None, eval_dir=opt.eval_dir, dataset_path=opt.dataset_path, target_size=opt.img_size, num_imgs=opt.num_imgs)
+    metrics_dict = calculate_metrics(opt.output_dir, opt.real_image_dir, cuda=True, isc=True, fid=True, kid=True, verbose=False)
+    print(metrics_dict)
